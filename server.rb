276bad0f4d72dd169fb65c43e539513b286fb139
Victor Mours <victor.mours@gmail.com>
2016-02-23 20:09:54 +0100

    Filter with params

--- server.rb
+++ server.rb
@@ -4,8 +4,23 @@ require 'rack'
 file = File.read('db.json')
 my_hash = JSON.parse(file)
 
+
+
 app = Proc.new do |env|
-  ['200', {'Content-Type' => 'application/json'}, [my_hash.to_json]]
+  request = Rack::Request.new(env)
+
+  if request.path == "/kings"
+    result = my_hash["kings"].select do |king|
+      request.params.map do |key, value|
+        king[key].to_s == value.to_s
+      end.reduce(:&)
+    end
+
+    ['200', {'Content-Type' => 'application/json'}, [result.to_json]]
+  else
+    ['200', {'Content-Type' => 'application/json'}, [my_hash.to_json]]
+  end
+
 end
 
 Rack::Handler::WEBrick.run app
